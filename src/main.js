68c3aefba9b18ef861c7fe76eb6515b8228bf6d8
nurgaleevif <nurgaleevif@pik.ru>
2022-10-10 17:31:16 +0500

    Подключил иконки

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {createPinia} from "pinia";
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 import 'element-plus/dist/index.css'
@@ -13,6 +14,11 @@ import router from "@/router";
 const pinia = createPinia()
 
 const app = createApp(App)
+
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(router)
     .use(ElementPlus,{ locale: ru })
